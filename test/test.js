78ee41114bcdb80fa9cf096998e430da0b57448c
stefounet <s@agilitik.com>
2011-07-18 14:41:21 +0200

    add some tests

--- test/test.js
+++ test/test.js
@@ -3,5 +3,6 @@ je teste qu'un domaine notfasterized.com donne bien l'origine
 je teste qu'une query de type A renvoie un enreg
 je checke qu'une query de type AAAA renvoie un enreg 
 je checke qu'une query de type NS renvoie un enreg
-je checke qu'une query en local donne mon serveur local
+je checke qu'une query d'un domaine en fasterized.com en local donne mon serveur local (127.0.0.1)
+je checke qu'une query d'un domaine autre que fasterized.com en local donne bien le bon enregistrement
 je foire, je continue de renvoyer un record
