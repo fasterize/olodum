fcd30877780f77e238342ddb11415568d59d6b52
stefounet <s@fasterize.com>
2012-06-05 17:45:16 +0200

    Cleanings

--- bench/bench.js
+++ bench/bench.js
@@ -1,29 +1,18 @@
-/*var exec = require('child_process').exec
-
-
-for (var i=0 ; i < MAX ; i++) {
-    exec('host ' + domains[j] + ' ' + NSSERVER, function (err,stdout) {
-	//nothing	
-	});
-}
-*/
 var dns = require('dns');
 var MAX = 100000 || argv[0];
 var startTime = new Date().getTime();
 var count = 1;
 var count_err = 0;
 for (var i = 0; i < MAX; i++) {
-		var my_name = 'toto.fasterized.net';
-		// my_name = 'www.google.com';
+		var my_name = 'www.google.com';
 		dns.resolve(my_name, 'A',function (err,addr) {
 			if (err) {count_err++}
-			// console.log('toto' + my_i + '.fasterized.com : ' + JSON.stringify(addr));
 			count++;
 			if (count === MAX) {
 				endTime = new Date().getTime();
 				console.log ('end : ' + (endTime - startTime));
-				console.log ('req/s: ' + ( MAX / ((endTime - startTime)/1000)))
-				console.log ('err :' + count_err)
+				console.log ('req/s: ' + ( MAX / ((endTime - startTime)/1000));
+				console.log ('err :' + count_err);
 			}
 		});
 };
--- test-runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd test
-
-for test in `ls`; do
-	echo "Launching test : $test"
-	vows $test --spec
-	echo ""
-done
